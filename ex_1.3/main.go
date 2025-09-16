58e11615928467dbc64dd6d4699bee7e56f0ba84
daniilt23 <tovstukhadaniil@gmail.com>
2025-09-16 23:10:32 +0300

    add ex3

--- ex_1.3/main.go
+++ ex_1.3/main.go
@@ -1,1 +1,24 @@
-package ex13
+package main
+
+import "fmt"
+
+func main() {
+	var workerCount int
+	if _, err := fmt.Scanf("%d", &workerCount); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ch := make(chan bool)
+	for i := 0; i < workerCount; i++ {
+		go func(ch chan bool) {
+			for val := range ch {
+				fmt.Printf("Worker %d take data %t\n", i, val)
+			}
+		}(ch)
+	}
+
+	for {
+		ch <- true
+	}
+}
