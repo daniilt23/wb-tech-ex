553f273e20b2df1534344f45535cd6dabf3ff80a
daniilt23 <tovstukhadaniil@gmail.com>
2025-09-20 19:56:03 +0300

    add ex1.7

---
+++ ex_1.7/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeCounter struct {
+	m  map[string]int
+	mu sync.Mutex
+}
+
+func (sc *SafeCounter) safeIncrement(key string) {
+	sc.mu.Lock() // блокируем для доступа только одной горутины
+	sc.m[key]++
+	sc.mu.Unlock() // разблокирование для прибавления счетчика
+}
+
+func (sc *SafeCounter) getValue(key string) int {
+	return sc.m[key]
+}
+
+func main() {
+	sc := SafeCounter{m: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go sc.safeIncrement("some_key")
+	}
+
+	time.Sleep(time.Second)              // сделаем ожидание чтобы горутины выполнились
+	fmt.Println(sc.getValue("some_key")) // проверяем что все прибавилось правильно выведет 1000
+}
