1a95cf84e6f967a168a75c239762c8ad1683ea91
daniilt23 <tovstukhadaniil@gmail.com>
2025-09-21 17:55:07 +0300

    add ex1.11

---
+++ ex_1.11/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	sl1 := []int{1, 2, 3, 4, 5, 6, 7}
+	sl2 := []int{2, 3, 4, 7}
+	set := make(map[int]struct{})
+	final := []int{}          // создаем слайс для ответа
+	for _, val := range sl1 { // добавляем первый слайс в мапу
+		set[val] = struct{}{}
+	}
+
+	for _, val := range sl2 {
+		if _, ok := set[val]; ok { // проверяем наличие ключей, если такие есть то повторение
+			final = append(final, val)
+		}
+	}
+
+	fmt.Println(final) // [2 3 4 7]
+}
