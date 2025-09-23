a24a21695ae897d4d766f2b8b9667609c903b50a
daniilt23 <tovstukhadaniil@gmail.com>
2025-09-23 18:55:51 +0300

    add ex_1.15

---
+++ ex_1.15/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "strings"
+
+var justString string
+
+func someFunc() {
+	v := createHugeString(1024)
+	justString = v[:100] // при таком присвоении у нас v не очистится пока не очистится justString
+	// нам нужно присваивать 100 байтов в новую переменную которая не держит указатель на старый слайс чтобы он очистился
+	str := make([]byte, 100)
+	copy(str, v[:100]) // копируем данные в новую переменную
+}
+
+func createHugeString(size int) string {
+	return strings.Repeat("x", size) // создаем строку весом 1024 байта
+}
+
+func main() {
+	someFunc()
+}
