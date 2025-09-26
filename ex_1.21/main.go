01619f9924e2fdb161609b7056ceb801271f7ab1
daniilt23 <tovstukhadaniil@gmail.com>
2025-09-26 18:15:58 +0300

    add ex_1.21

--- ex_1.21/main.go
+++ ex_1.21/main.go
@@ -1,5 +1,41 @@
 package main
 
+import "fmt"
+
+type Payment interface { // у нас есть интерфейс для оплаты
+	ProcessPayment()
+}
+
+type PaymentProcessor struct{}
+
+func (pp PaymentProcessor) ProcessPayment() {
+	// любая бизнес логика
+	fmt.Println("Make payment with old system")
+}
+
+// мы хотим интегрировать новый способ оплаты от внешнего сервиса который мы не можем менять
+type NewPaymentProcessor struct{}
+
+func (npp NewPaymentProcessor) NewProcessPayment() {
+	// любая бизнес логика
+	fmt.Println("Make payment with new system")
+}
+
+// создаем структуру которая включает полностью новый интерфейс
+type NewPaymentAdapter struct {
+	newPaymentProcessor *NewPaymentProcessor
+}
+
+func (npa NewPaymentAdapter) ProcessPayment() {
+	npa.newPaymentProcessor.NewProcessPayment()
+}
+
 func main() {
-	
+	var p Payment // единый интерфейс для всех оплат
+
+	p = &PaymentProcessor{}
+	p.ProcessPayment() // Make payment with old system
+
+	p = &NewPaymentAdapter{newPaymentProcessor: &NewPaymentProcessor{}}
+	p.ProcessPayment() // Make payment with new system
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module wb
-
-go 1.25.1
